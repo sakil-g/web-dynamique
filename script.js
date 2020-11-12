1473f0842b6a64f62da50c8ccb36c8c274c06c2d
Sakil GOULAMALY <sakil113@gmail.com>
2020-11-12 08:41:58 +0400

    Update script.js
    add animation section services

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var swiper = new Swiper('.swiper-container', {
 
     $(window).scroll(function() { // Says this function is preformed continuisly while scrolling.
         var Scroll = $(window).scrollTop() + 1, // This variable finds the distance you have scrolled from the top.
-                    SectionWhere = $('#wherebefore').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
+                    SectionWhere = $('#services').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
 
         if (Scroll >= SectionWhere) { 
             AOS.init();
